f43fced8b27e3019f9216e09d0f0bef1314aff8c
dupp <dupp@wasu.com>
2020-10-12 17:39:59 +0800

    optimize, bug fix

--- src/handler/leases_handler.rs
+++ src/handler/leases_handler.rs
@@ -1,6 +1,10 @@
+use std::ops::Deref;
+
 use actix_web::{HttpResponse, web};
 use serde::Deserialize;
 use serde_json::json;
+
+use crate::handler::Resp;
 
 #[allow(non_snake_case)]
 #[derive(Deserialize)]
@@ -14,35 +18,34 @@ pub struct FormData {
 
 // POST /jrebel/leases
 pub async fn lease(params: web::Form<FormData>) -> HttpResponse {
-    if let None = params.username {
-        return HttpResponse::Forbidden().finish();
+    match params.deref() {
+        FormData { username: None, .. } =>
+            HttpResponse::Ok().json(Resp::<()>::failure("username can not be null")),
+        FormData { randomness: None, .. } =>
+            HttpResponse::Ok().json(Resp::<()>::failure("randomness can not be null")),
+        FormData { guid: None, .. } =>
+            HttpResponse::Ok().json(Resp::<()>::failure("guid can not be null")),
+        FormData { .. } =>
+            HttpResponse::Ok().json(json!({
+                "company": params.username,
+                "evaluationLicense": false,
+                "groupType": "managed",
+                "id": 1,
+                "licenseType": 1,
+                "licenseValidFrom": 1490544001000u64,
+                "licenseValidUntil": 1691839999000u64,
+                "offline": params.offline,
+                "orderId": "",
+                "seatPoolType": "standalone",
+                "serverGuid": "a1b4aea8-b031-4302-b602-670a990272cb",
+                "serverProtocolVersion": "1.1",
+                "serverRandomness": "H2ulzLlh7E0=",
+                "serverVersion": "3.2.4",
+                "signature": "", // TODO
+                "statusCode": "SUCCESS",
+                "validFrom": None::<String>,
+                "validUntil": None::<String>,
+                "zeroIds": []
+            })),
     }
-    if let None = params.randomness {
-        return HttpResponse::Forbidden().finish();
-    }
-    if let None = params.guid {
-        return HttpResponse::Forbidden().finish();
-    }
-
-    return HttpResponse::Ok().json(json!({
-        "company": params.username,
-        "evaluationLicense": false,
-        "groupType": "managed",
-        "id": 1,
-        "licenseType": 1,
-        "licenseValidFrom": 1490544001000u64,
-        "licenseValidUntil": 1691839999000u64,
-        "offline": params.offline,
-        "orderId": "",
-        "seatPoolType": "standalone",
-        "serverGuid": "a1b4aea8-b031-4302-b602-670a990272cb",
-        "serverProtocolVersion": "1.1",
-        "serverRandomness": "H2ulzLlh7E0=",
-        "serverVersion": "3.2.4",
-        "signature": "", // TODO
-        "statusCode": "SUCCESS",
-        "validFrom": Option::<String>::None,
-        "validUntil": Option::<String>::None,
-        "zeroIds": []
-    }));
 }
--- src/handler/mod.rs
+++ src/handler/mod.rs
@@ -1,5 +1,26 @@
+use actix_web::web::Data;
+use serde::Serialize;
+use serde_json::json;
+
 pub mod index_handler;
 pub mod leases_handler;
 pub mod ticket_handler;
 pub mod validate_handler;
 pub mod ping_handler;
+
+#[derive(Serialize)]
+pub struct Resp<T: Serialize> {
+    code: i8,
+    msg: &'static str,
+    data: Option<T>,
+}
+
+impl<T> Resp<T> where T: Serialize {
+    fn success(d: T) -> Self {
+        Resp { code: 0, msg: "ok", data: Some(d) }
+    }
+
+    fn failure(s: &'static str) -> Self {
+        Resp { code: -1, msg: s, data: None }
+    }
+}
