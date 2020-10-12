8bc01d6ae90d741f42020260f2725c29a3d4bda7
dupp <dupp@wasu.com>
2020-10-12 13:03:22 +0800

    update

--- src/handler/leases_handler.rs
+++ src/handler/leases_handler.rs
@@ -1,5 +1,5 @@
-use actix_web::{web, HttpResponse};
-use serde::{Deserialize, Serialize};
+use actix_web::{HttpResponse, web};
+use serde_json::json;
 
 // POST /jrebel/leases
 pub async fn lease(params: web::Form<FormData>) -> HttpResponse {
@@ -13,29 +13,30 @@ pub async fn lease(params: web::Form<FormData>) -> HttpResponse {
         return HttpResponse::Forbidden().finish();
     }
 
-    return HttpResponse::Ok().json(RespData {
-        company: params.username.as_deref().unwrap_or("").to_string(),
-        evaluationLicense: false,
-        groupType: "managed".to_string(),
-        id: 1,
-        licenseType: 1,
-        licenseValidFrom: 1490544001000,
-        licenseValidUntil: 1691839999000,
-        offline: params.offline.to_string(),
-        orderId: "".to_string(),
-        seatPoolType: "standalone".to_string(),
-        serverGuid: "a1b4aea8-b031-4302-b602-670a990272cb".to_string(),
-        serverProtocolVersion: "1.1".to_string(),
-        serverRandomness: "H2ulzLlh7E0=".to_string(),
-        serverVersion: "3.2.4".to_string(),
-        signature: "".to_string(), // TODO
-        statusCode: "SUCCESS".to_string(),
-        validFrom: None,
-        validUntil: None,
-        zeroIds: [],
+    return HttpResponse::Ok().json(json! {
+        "company": params.username.as_deref().unwrap_or("").to_string(),
+        "evaluationLicense": false,
+        "groupType": "managed".to_string(),
+        "id": 1,
+        "licenseType": 1,
+        "licenseValidFrom": 1490544001000,
+        "licenseValidUntil": 1691839999000,
+        "offline": params.offline.to_string(),
+        "orderId": "".to_string(),
+        "seatPoolType": "standalone".to_string(),
+        "serverGuid": "a1b4aea8-b031-4302-b602-670a990272cb".to_string(),
+        "serverProtocolVersion": "1.1".to_string(),
+        "serverRandomness": "H2ulzLlh7E0=".to_string(),
+        "serverVersion": "3.2.4".to_string(),
+        "signature": "".to_string(), // TODO
+        "statusCode": "SUCCESS".to_string(),
+        "validFrom": None,
+        "validUntil": None,
+        "zeroIds": [],
     });
 }
 
+#[allow(non_snake_case)]
 #[derive(Deserialize)]
 pub struct FormData {
     username: Option<String>,
@@ -43,27 +44,4 @@ pub struct FormData {
     guid: Option<String>,
     offline: bool,
     clientTime: String,
-}
-
-#[derive(Serialize)]
-pub struct RespData {
-    company: String,
-    evaluationLicense: bool,
-    groupType: String,
-    id: u32,
-    licenseType: u32,
-    licenseValidFrom: u64,
-    licenseValidUntil: u64,
-    offline: String,
-    orderId: String,
-    seatPoolType: String,
-    serverGuid: String,
-    serverProtocolVersion: String,
-    serverRandomness: String,
-    serverVersion: String,
-    signature: String,
-    statusCode: String,
-    validFrom: Option<String>,
-    validUntil: Option<String>,
-    zeroIds: [String; 0],
 }
