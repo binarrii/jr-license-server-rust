4e3be80db42a03d405e7d8989a081185543627fe
dupp <dupp@wasu.com>
2022-02-08 17:20:31 +0800

    update: optimize

--- src/handler/index_handler.rs
+++ src/handler/index_handler.rs
@@ -1,11 +1,6 @@
-use actix_web::HttpRequest;
+use actix_web::{HttpRequest, Responder};
 
 #[get("/")]
-pub async fn index(_req: HttpRequest) -> &'static str {
-    r"
-    <h1>Hello,This is a Jrebel & JetBrains License Server!</h1>
-    <p>License Server started at</p>;
-    <p>JRebel 7.1 and earlier version Activation address was: <span style='color:red'></span>, with any email.</p>
-    <p>JRebel 2018.1 and later version Activation address was: , with any email.</p>
-    "
+pub async fn index(_req: HttpRequest) -> impl Responder {
+    r"<h1>Hello,This is a Jrebel & JetBrains License Server!</h1>"
 }
