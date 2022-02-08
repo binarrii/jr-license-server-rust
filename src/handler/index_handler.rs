f85bf19063967be86b17d996c857d14a99b98889
dupp <dupp@wasu.com>
2022-02-08 17:47:16 +0800

    update: optimize

--- src/handler/index_handler.rs
+++ src/handler/index_handler.rs
@@ -4,5 +4,5 @@ use actix_web::{HttpRequest, HttpResponse};
 pub async fn index(_req: HttpRequest) -> HttpResponse {
     HttpResponse::Ok()
         .content_type("text/html")
-        .body(r"<h1>Hello,This is a Jrebel & JetBrains License Server!</h1>")
+        .body(r"<h3>Hello, This is a Jrebel & JetBrains License Server!</h3>")
 }
